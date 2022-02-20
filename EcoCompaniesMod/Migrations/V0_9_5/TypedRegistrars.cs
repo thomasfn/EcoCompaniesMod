72fccff816685a453766edccac286937cb9558c7
Thomas Smith <thomasfn@fortfn.co.uk>
2022-02-20 19:58:37 +0000

    Added migrations for 0.9.5.0.

---
+++ EcoCompaniesMod/Migrations/V0_9_5/TypedRegistrars.cs
@@ -0,0 +1,32 @@
+﻿namespace Eco.Mods.Companies.Migrations.V0_9_5
+{
+    using Eco.Core.Serialization.Migrations;
+    using Eco.Core.Serialization.Migrations.Attributes;
+    using Eco.Core.Systems;
+
+    [Migration(SinceVersion = 3.915f)]
+    [MigrationType(typeof(Registrar<Company>))]
+    public class TypedRegistrarMigration : AggregateMigration
+    {
+        public TypedRegistrarMigration()
+        {
+            this.MigrateRegistrars();
+        }
+
+        private void MigrateRegistrars()
+        {
+            this.AddDataMigration("class[Eco.Mods.Companies.CompaniesData]", dm =>
+            {
+                this.MigrateRegistrar(dm, "Companies", "class[Eco.Mods.Companies.Company]");
+            });
+        }
+
+        private void MigrateRegistrar(DataMigration dataMigration, string memberName, string registarEntrySchemaType)
+        {
+            var memberMigration = new DataMigration("Eco.Core.Systems.Registrar");
+            memberMigration.ChangeMemberSchemaType("IdToObj", SchemaUtils.MakeDictionarySchemaType(SchemaUtils.GetSchemaType(typeof(int)), registarEntrySchemaType));
+            memberMigration.ChangeSchemaType(SchemaUtils.MakeGenericClassSchemaType("Eco.Core.Systems.Registrar", registarEntrySchemaType));
+            dataMigration.MigrateMember(memberName, memberMigration, skipIfMissing: true);
+        }
+    }
+}
